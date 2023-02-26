bd64a432c9a9673dbce55fdb4c99b407d737b2ea
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-26 09:32:56 +0100

    Animation

---
+++ unity-animation/Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public bool isInverted;
+    //-----Privates variables-----\\
+    private Vector3 offset;
+
+    //-----Publics variables-----\\
+    [Header("Variables")]
+    public Transform player;
+
+    [Space]
+    [Header("Position")]
+    public float camPosX;
+    public float camPosY;
+    public float camPosZ;
+
+    [Space]
+    [Header("Rotation")]
+    public float camRotationX;
+    public float camRotationY;
+    public float camRotationZ;
+
+    [Space]
+    [Range(0f, 10f)]
+    public float turnSpeed;
+
+    //-----Privates functions-----\\
+    private void Start()
+    {
+        offset = new Vector3(player.position.x + camPosX, player.position.y + camPosY, player.position.z + camPosZ);
+        transform.rotation = Quaternion.Euler(camRotationX, camRotationY, camRotationZ);
+    }
+
+
+    private void LateUpdate()
+    {
+        if (!PauseMenu.GameIsPause)
+        {
+            if (isInverted)
+                offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(-Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offset;
+            else
+                offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offset;
+            transform.position = player.position + offset;
+            transform.LookAt(player.position);
+        }
+    }
+}
---
+++ unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public GameObject MainCamera;
+    public GameObject Player;
+    public GameObject TimerCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void camTransition()
+    {
+        MainCamera.SetActive(true);
+        TimerCanvas.SetActive(true);
+        Player.GetComponent<PlayerController>().enabled = true;
+        gameObject.SetActive(false);
+    }
+}
